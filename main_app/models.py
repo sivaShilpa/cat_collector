6fc258fb309c50d42470ae4cda3486cc65543843
sivaShilpa <shilpa.vchand@gmail.com>
2023-04-14 10:24:15 -0400

    1:M

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.urls import reverse
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
 
 # Create your models here.
+class Cat(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'cat_id': self.id})
+    
+    # Add new Feeding model below Cat model
+class Feeding(models.Model):
+  date = models.DateField('feeding date')
+  meal = models.CharField(max_length=1,
+    # add the 'choices' field option
+    choices=MEALS,
+    # set the default value for meal to be 'B'
+    default=MEALS[0][0]
+  )
+  cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+  def __str__(self):
+    # Nice method for obtaining the friendly value of a Field.choice
+    return f"{self.get_meal_display()} on {self.date}"
+  
+  class Meta:
+    ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Cat
+from .forms import FeedingForm
 # views.py
 
 # Add this cats list below the imports
-cats = [
-  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
-  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
-]
+# cats = [
+#   {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+#   {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+# ]
 
 # Create your views here.
 # Define the home view
@@ -18,4 +20,45 @@ def about(request):
   return render(request, 'about.html')
 
 def cats_index(request):
+  cats = Cat.objects.all()
   return render(request, 'cats/index.html', {'cats': cats})
+
+def cats_detail(request, cat_id):
+  cat = Cat.objects.get(id=cat_id)
+  feeding_form = FeedingForm()
+  return render(request, 'cats/detail.html', { 'cat': cat, 'feeding_form': feeding_form})
+
+class CatCreate(CreateView):
+  model = Cat
+  fields = '__all__'
+  # Special string pattern Django will use
+  # success_url = '/cats/{cat_id}' <- what it may look like under hood
+  # Or if you wanted to redirect to the index page
+  # success_url = '/cats'
+
+class CatUpdate(UpdateView):
+  model = Cat
+  # Let's disallow the renaming of a cat by excluding the name field!
+  fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+  model = Cat
+  success_url = '/cats'
+
+...
+
+# add this new function below cats_detail
+def add_feeding(request, cat_id):
+  # create a ModelForm instance using the data in request.POST
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # don't save the form to the db until it
+    # has the cat_id assigned
+    new_feeding = form.save(commit=False)
+    new_feeding.cat_id = cat_id
+    new_feeding.save()
+  return redirect('detail', cat_id=cat_id)
+
+
+
